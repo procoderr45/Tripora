accc6f9d0c4472f9a7b29cea0490b85b167c6ade
Om Takbhate <omtakbhate007@gmail.com>
2025-10-20 17:18:46 +0530

    chore(db): setup db to connect to database

---
+++ src/config/db.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import getEnvVariable from "./env";
+
+async function connectToDb() {
+    const MONGO_URI = getEnvVariable("MONGO_URI");
+
+    await mongoose.connect(MONGO_URI);
+}
+
+export default connectToDb;
