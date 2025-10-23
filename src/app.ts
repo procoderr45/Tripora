c2161d79614ba58c6f2a39f8c971c20afd78802c
Om Takbhate <omtakbhate007@gmail.com>
2025-10-23 14:02:16 +0530

    fix(cors): resolve cors error

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,24 @@ import isLoggedIn from "./middlewares/auth/isLoggedIn.js";
 import cookieParser from "cookie-parser";
 import planRouter from "./routes/plan.route.js";
 import itinenaryRouter from "./routes/itinenary.route.js";
+import cors from "cors";
 
 const app: Express = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
 app.use("/profile", isLoggedIn, profileRouter);
-app.use("/plan", isLoggedIn, planRouter);
-app.use("/itinenary", isLoggedIn, itinenaryRouter);
+app.use("/plan",  planRouter);
+app.use("/itinenary", itinenaryRouter);
 
 app.use(errorHandler);
 
